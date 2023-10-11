f5a10e4bb0713b353b31f26d5bea5b7c36afe189
bjovanovic84 <bojan@polycam.ai>
2023-10-11 17:50:37 -0400

    overlay ports

--- ports/filament/filamentConfig.cmake
+++
@@ -1,8 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(ZLIB)
-find_dependency(PNG)
-find_dependency(stb CONFIG)
-find_dependency(draco CONFIG)
-find_dependency(assimp CONFIG)
-
-include("${CMAKE_CURRENT_LIST_DIR}/filamentTargets.cmake")
